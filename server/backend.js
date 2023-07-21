75d11e502d72af564d500070ddc1add5242cdf5a
Sahil <sahifindmyphone@gmail.com>
2023-07-21 15:31:55 +0530

    auth hashing

--- server/backend.js
+++ server/backend.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const cors = require('cors')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const app = express()
 const port = process.env.PORT || 8000;
 const validator = require('validator')
@@ -9,18 +10,70 @@ app.use(cors())
 
 const collection = require('./mongo');
 
+// Middleware to verify the token in the request headers
+const verifyToken = (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        console.log("No token provided");
+        return res.status(401).json({ message: 'No token provided' });
+    }
+
+    jwt.verify(token.split(' ')[1], process.env.SECRET_KEY, (err, decoded) => {
+        if (err) {
+            console.log("Invalid token:", err);
+            return res.status(403).send('Invalid token');
+        }
+
+        // console.log("Decoded token:", decoded); // Log the decoded payload for debugging
+
+        req.decoded = decoded; // Store the decoded user information in the request object
+        next();
+    });
+};
+
+
+
+app.get('/note', verifyToken, async (req, res) => {
+    try {
+        // console.log(req.decoded)
+        const userId = req.decoded._id;
+        // console.log(userId)
+        const doc = await collection.findOne({ _id: userId })
+
+        // console.log(doc)
+
+        res.send(doc)
+    } catch (error) {
+
+    }
+})
+
 app.post("/", async (req, res) => {
-    const user = req.body;
+    const { email, password } = req.body;
     // console.log(user);
 
     try {
-        const check1 = await collection.findOne({ email: user.email });
-        if (check1) {
-            if (check1.password === user.password) {
-                res.send(check1);
-            } else {
+        const doc = await collection.findOne({ email: email })
+
+        if (doc) {
+            //hashing check
+            const isMatch = await bcrypt.compare(password, doc.password)
+
+
+            if (!isMatch)
                 res.send("wrongpassword");
+            else {
+
+                // authentication
+                const token = await doc.generateAuthToken()
+                // console.log(token)
+
+                // res.send({ data: token, message: "login" })
+                res.send(token)
+
             }
+
         } else {
             // console.log("email does not exist in backend");
             res.send("notexist");
@@ -38,21 +91,25 @@ app.post("/signup", async (req, res) => {
     if (!isValidEmail) {
         return res.send("Invalid email address");
     }
-    if (!(user.password === user.cpassword)) {
-        console.log(user)
-        return res.send("notmatch")
-    }
-
-
     try {
         const check = await collection.findOne({ email: user.email });
 
         if (check) {
             return res.send("alreadyexist");
         } else {
-            const added = await collection.insertMany([user]);
+            // const added = await collection.insertMany([user]);
             // console.log(added);
-            res.send(added[0]);
+            // res.send(added[0]);
+
+            const user1 = new collection(user)
+            const doc = await user1.save();
+
+            // authentication
+            const token = await doc.generateAuthToken()
+            // console.log(token)
+
+            res.send(token)
+
         }
     } catch (err) {
         // console.log(err);
@@ -86,19 +143,19 @@ app.post("/note/:email", async (req, res) => {
 
 })
 
-app.get("/note/:email", async (req, res) => {
+// app.get("/note/:email", async (req, res) => {
 
-    const email = req.params.email
-    // console.log("email from frontend " + email)
-    try {
-        const doc = await collection.findOne({ email: email })
-        // console.log(email)
-        // console.log(doc.notes)
-        res.send(doc.notes)
-    } catch (err) {
-        console.log("error while note get : " + err)
-    }
-})
+//     const email = req.params.email
+//     // console.log("email from frontend " + email)
+//     try {
+//         const doc = await collection.findOne({ email: email })
+//         // console.log(email)
+//         // console.log(doc.notes)
+//         res.send(doc.notes)
+//     } catch (err) {
+//         console.log("error while note get : " + err)
+//     }
+// })
 
 app.delete("/note/:email/:id", async (req, res) => {
     const email = req.params.email
@@ -127,7 +184,7 @@ app.patch("/note/:email", async (req, res) => {
         doc.notes[id] = updatedText
         doc.save()
         // console.log("updated")
-        res.send("updated")
+        res.send(doc.notes)
     } catch (err) {
         console.log("error while note patch : " + err)
 
