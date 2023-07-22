0ff2b41ee3b62173fc3312787d7a04876d114861
Sahil Gupta <83276402+sahil974@users.noreply.github.com>
2023-07-22 18:22:55 +0530

    Update url.js

--- client/src/component/url.js
+++ client/src/component/url.js
@@ -1,3 +1,3 @@
-// const BASE_URL = "https://note-app-da8z.onrender.com"
-const BASE_URL = "http://localhost:8000"
+const BASE_URL = "https://note-app-da8z.onrender.com"
+// const BASE_URL = "http://localhost:8000"
 module.exports = BASE_URL;
